4fc002b57e3d1eecf9d40e63ebba86126ccd56a1
abiwrigh <112552934+abiwrigh@users.noreply.github.com>
2023-03-20 14:59:22 -0400

    Update DataBase.php

--- lib/DataBase.php
+++ lib/DataBase.php
@@ -6,6 +6,16 @@ class DataBase{
     $this->pdo = null;
     $dsn = 'mysql:host=webdb.uvm.edu;dbname=' . $dataBaseName;
     $DataBasePassword= '';
+    switch (n) {
+  case netid_reader:
+    $DataBasePassword=;
+    break;
+  case netid_writer:
+    code to be executed if n=label2;
+    break;
+  default:
+    code to be executed if n is different from all labels;
+}
   }
 }
 ?>
